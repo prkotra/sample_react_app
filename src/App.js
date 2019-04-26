28cfafaa906821ab6f5fd93d6f6200ee1fee8bfc
prkotra <prkotra@gmail.com>
2019-04-26 15:16:18 +0000

    second deployment

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <p> Welcome to  ReactJS application Deployment!!!!!!</p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
